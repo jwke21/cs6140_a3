075e0862666ec05d67cc250820a77a7cfbca534c
jwke21 <jakevm21@gmail.com>
2023-02-24 16:54:19 -0800

    Updated main

--- main.py
+++ main.py
@@ -20,7 +20,27 @@ def main():
     #################### Classification with iteration ####################
     if "all" in argv or "2" in argv:
         cls.main()
-
+    #################### KNN ####################
+    if "knn" in argv:
+        cls.knn_classification()
+    #################### Logistic Regression ####################
+    if "lr" in argv:
+        cls.logistic_regression_classifion()
+    #################### SVM ####################
+    if "svm" in argv:
+        cls.svm_classification_with_iterations()
+    #################### Multinomial Naive Bayes ####################
+    if "mnb" in argv:
+        cls.multinomial_nb_classification()
+    #################### Bernoulli Naive Bayes ####################
+    if "bnb" in argv:
+        cls.bernoulli_nb_classification()
+    #################### Gaussian Naive Bayes ####################
+    if "gnb" in argv:
+        cls.gaussian_nb_classification_with_iterations()
+    #################### Decision Tree ####################
+    if "dt" in argv:
+        cls.decision_tree_classification()
 
 if __name__ == "__main__":
     main()
